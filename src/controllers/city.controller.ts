e8e20613fc9469c0f1afa01661725e53d98be0f7
alx-b <>
2021-05-22 18:47:33 +0200

    Added city controller.

---
+++ src/controllers/city.controller.ts
@@ -0,0 +1,35 @@
+import CityModel from "../models/city.model"
+import express from "express"
+import HttpStatus from "../httpStatusCode"
+
+
+const getCities = async (_: express.Request, res: express.Response) => {
+    try {
+        const cities = await CityModel.find()
+        res.status(HttpStatus.OK).send(cities)
+    } catch (error) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: error.message })
+    }
+}
+
+const addCity = async (req: express.Request, res: express.Response) => {
+    const city = new CityModel({
+        name: req.body.name,
+        state: req.body.state,
+        country: req.body.country,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude
+    })
+
+    try {
+        const newCity = await city.save()
+        res.status(HttpStatus.CREATED).send(newCity)
+    } catch (error) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({message: error.message})
+    }
+}
+
+export default {
+    getCities,
+    addCity
+}
